28d59ed2cf5093b600d619542aff7a8c0fd6f28a
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-21 08:59:12 +0900

    Update and rename generator.py to decoration_helper.py

---
+++ decoration_helper.py
@@ -0,0 +1,16 @@
+from decorators import class_info, member_info
+from meta_info_store import MetaInfoStore
+
+
+class DecorationHelper:
+    def __init__(self, name):
+        self._name = name
+        self._store = MetaInfo
+
+    def get_store(self, cls):
+        return getattr(cls, seelf.name, None)
+    
+    def class_info(self, info):
+        return class_info(store, info)
+
+    def member_info(self, info):
--- generator.py
+++
@@ -1,6 +0,0 @@
-from decorators import class_info, member_info
-from meta_info_store import MetaInfoStore
-
-
-class MetaInfoGenerator:
-    pass
