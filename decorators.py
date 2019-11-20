da082e23654c34ca5d6949e09433de95248a518e
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-20 18:42:23 +0900

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,7 +1,32 @@
+from meta_info_store import MetaInfoStore
 
-def class_info():
-    pass
+_TEMPORARY_STORE = {}
+""" :type: typing.Dict[str, MetaInfoStore] """
 
 
-def prop_info():
-    pass
+def class_info(store, info):
+    def decorator(cls):
+        
+        return cls
+    return decorator
+
+
+def member_info(store, info):
+    st = _get_store(store)
+    def decorator(member):
+        st.member_info[member.__name__] = info
+        return member
+    return decorator
+
+
+def _get_store(store):
+    if isinstance(store, str):
+        st = _TEMPORARY_STORE.get(store)
+        if st is None:
+            st = MetaInfoStore()
+            _TEMPORARY_STORE[store] = st
+        return st
+    elif isinstance(store, MetaInfoStore):
+        return store
+    else:
+        raise TypeError(f"store must be str or MetaInfoStore but got: {type(store)}")
