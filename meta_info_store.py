fb4c17712a6cde0afe6f24f3e4ecc358b6407d2a
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-20 18:15:41 +0900

    Update meta_info_store.py

--- meta_info_store.py
+++ meta_info_store.py
@@ -1,2 +1,10 @@
-class MetaInfoStore:
-    pass
+from typing import TypeVar, Generic, Dict
+
+C = TypeVar("C")
+P = TypeVar("P")
+
+
+class MetaInfoStore(Generic[C, P]):
+    def __init__(self):
+        self.cls_info: C = None
+        self.prop_info: Dict[str, P] = {}
