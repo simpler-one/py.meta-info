b05d5ef27cdcdfb7272b0dfc7762fc15534546fa
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-21 12:52:43 +0900

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,18 +1,18 @@
 from meta_info_store import MetaInfoStore
 
 
-def class_info(store, info, on_complete):
-    def decorator(cls):
-        if callable(on_complete):
-            on_complete(cls)
-        return cls
+def notify_decoration(on_decorate):
+    def decorator(target):
+        if callable(on_decorate):
+            on_decorate(target)
+        return target
     return decorator
 
 
 def member_info(store, info):
     def decorator(member):
         func = member.fget if isinstance(member, property) else member
-        store.member_info[func.__name__] = info
+        store.members[func.__name__] = info
         return member
     return decorator
 
