1a2f59ac1c19d8b3023c122dccd64b773527d5a6
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-21 12:38:49 +0900

    Update decoration_helper.py

--- decoration_helper.py
+++ decoration_helper.py
@@ -2,16 +2,26 @@ from decorators import class_info, member_info
 from meta_info_store import MetaInfoStore
 
 
+def nop(*_): pass
+
+
 class DecorationHelper:
-    def __init__(self, name):
+    def __init__(self, name, on_complete=nop):
         self._name = name
-        self._store = MetaInfo
+        self._store = MetaInfoStore()
+        self._complete_callback = on_complete
 
     def get_store(self, cls):
         return getattr(cls, seelf.name, None)
     
-    def class_info(self, info):
-        return class_info(store, info, True)
+    def class_info(self, info, on_complete):
+        return class_info(store, info, self.on_complete)
 
     def member_info(self, info):
         return member_info(store, info)
+
+    def _on_complete(self, cls):
+        store = self._store
+        setattr(cls, self._name, store)
+        self._store = MetaInfoStore()
+        self._complete_callback(cls, store)
