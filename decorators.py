8d8c6e4f4b828d5c6bb2bbd6ea4f422ef0d305d0
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-21 12:26:44 +0900

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,33 +1,19 @@
 from meta_info_store import MetaInfoStore
 
-_TEMPORARY_STORE = {}
-""" :type: typing.Dict[str, MetaInfoStore] """
 
-
-def class_info(store, info, use_internal):
+def class_info(store, info, on_complete):
     def decorator(cls):
-        if use_internal:
-            setattr(cls, store, st)
-            _TEMPORARY_STORE.delete(store)
+        if callable(on_complete):
+            on_complete(cls)
         return cls
     return decorator
 
 
 def member_info(store, info):
     def decorator(member):
-        st.member_info[member.__name__] = info
+        func = member.fget if isinstance(member, property) else member
+        store.member_info[func.__name__] = info
         return member
     return decorator
 
 
-def _get_store(store):
-    if isinstance(store, str):
-        st = _TEMPORARY_STORE.get(store)
-        if st is None:
-            st = MetaInfoStore()
-            _TEMPORARY_STORE[store] = st
-        return st
-    elif isinstance(store, MetaInfoStore):
-        return store
-    else:
-        raise TypeError(f"store must be str or MetaInfoStore but got: {type(store)}")
