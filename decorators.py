a007dca4ec46f631f3acf0159f72494307ccfa38
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-22 18:49:46 +0900

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,10 +1,11 @@
+from abc import ABC, abdtractmethod
 from meta_info_store import MetaInfoStore
 
 
 def notify_decoration(on_decorate):
     def decorator(target):
         if callable(on_decorate):
-            on_decorate(target)
+            on_decorate(target, target.__name__)
         return target
     return decorator
 
@@ -13,7 +14,13 @@ def member_info(store, info):
     def decorator(member):
         func = member.fget if isinstance(member, property) else member
         store.members[func.__name__] = info
+        if isinstance(info, OnDecorate):
+            info.on_edcorate(member, __name__)
         return member
     return decorator
 
 
+class OnDecotate(ABC):
+    @abstractmethod
+    def on_decorate(self, target, name):
+        raise NotImplementedError()
